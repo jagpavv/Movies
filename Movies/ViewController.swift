d3d4ac8fe9114d3946a199e3bae3452d6f2df118
Eunjin Kim <jagpavv@gmail.com>
2018-09-21 16:52:17 +0200

    Request API as a text

--- Movies/ViewController.swift
+++ Movies/ViewController.swift
@@ -1,10 +1,20 @@
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+  let parameters: Parameters = [
+    "api_key": "2696829a81b1b5827d515ff121700838",
+    "query": "batman",
+    "page": 1
+  ]
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    Alamofire.request("http://api.themoviedb.org/3/search/movie", parameters: parameters).responseString { (response) in
+      print(response)
+    }
   }
 
 
