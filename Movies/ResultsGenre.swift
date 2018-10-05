5d279c97705a1afd0bd3f5350839ecd7fc2e1a04
Eunjin Kim <jagpavv@gmail.com>
2018-10-05 17:44:29 +0200

    Add search Bar on viewController

---
+++ Movies/ResultsGenre.swift
@@ -0,0 +1,14 @@
+//import Alamofire
+//import SwiftyJSON
+//import Foundation
+//
+//struct ResultsGenre {
+//  let genre_ids: Array<Int>?
+//
+//  init(json: JSON) {
+//
+//    self.genre_ids = json["results"].arrayValue.map({$0["genre_ids"].intValue})
+//
+//    print("genre_ids: \(self.genre_ids)")
+//  }
+//}
--- Movies/ViewController.swift
+++ Movies/ViewController.swift
@@ -3,7 +3,13 @@ import Alamofire
 import SwiftyJSON
 import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var suggestList: UITableView!
+
+  var moiveData = [MovieInfo]()
+  let sugesstSearchResult = ["Batman", "Batman begins", "Batman & Robin"]
 
   let parameters: Parameters = [
     "api_key": "2696829a81b1b5827d515ff121700838",
@@ -14,26 +20,39 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    // get data from API
     Alamofire.request("http://api.themoviedb.org/3/search/movie", method: .get, parameters: parameters).validate().responseJSON { response in
       switch response.result {
       case .success(let value):
         let json = JSON(value)
-//        print("JSON: \(json)")
 
-        TotalInfo.init(json: json)
-
-        /* 이하 json["results"][num]["title"] -> results > title만 빼냄
-        let totalCount = json ["results"].count
-        print(totalCount)
-
-        for num in 0..<json ["results"].count {
-          let movieTitle = json["results"][num]["title"]
-          print("movieTitle \(num): \(movieTitle)")
+        for v in json["results"].arrayValue {
+          let a = MovieInfo.init(json: v)
+          self.moiveData.append(a)
         }
-*/
+        print(self.moiveData)
+
       case .failure(let error):
         print(error)
       }
     }
+  }
+
+  // searchBar
+  func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+    print("yeah")
+  }
+
+  // suggest list table view
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return sugesstSearchResult.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let sugesstSearchResult = self.sugesstSearchResult[indexPath.row]
+
+    let cell = tableView.dequeueReusableCell(withIdentifier: "suggestCell") as! SuggestCell
+    cell.fillLabel(data: sugesstSearchResult)
+    return cell
   }
 }
